58bc19a41d555487be32d3afb931afceece89c72
Harimohan Rajamohanan <30436665+harimohanr@users.noreply.github.com>
2023-06-10 12:43:50 +0530

    OpenKM dependency and jasypt removal (#4)
    Removed the ununsed opemkm dependency and the utility classes
    Removed the jasypt library for password encryption
    Added CryptoUtil which is the javax.crypto for password encryption

--- src/main/java/com/iemr/admin/utils/IEMRApplBeans.java
+++ src/main/java/com/iemr/admin/utils/IEMRApplBeans.java
@@ -6,109 +6,58 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.session.data.redis.config.annotation.web.http.RedisHttpSessionConfiguration;
 
 import com.iemr.admin.utils.config.ConfigProperties;
 import com.iemr.admin.utils.gateway.email.EmailService;
 import com.iemr.admin.utils.gateway.email.GenericEmailServiceImpl;
-import com.iemr.admin.utils.km.KMService;
-import com.iemr.admin.utils.km.openkm.OpenKMServiceImpl;
 import com.iemr.admin.utils.redis.RedisStorage;
 import com.iemr.admin.utils.sessionobject.SessionObject;
 import com.iemr.admin.utils.validator.Validator;
 
 @Configuration
-public class IEMRApplBeans
-{
-	@Bean
-	public KMService getOpenKMService()
-	{
-		KMService kmService = new OpenKMServiceImpl();
-		return kmService;
-	}
+public class IEMRApplBeans {
+
+	private @Value("${spring.redis.host}") String redisHost;
+	private @Value("${spring.redis.port}") int redisPort;
 
 	@Bean
-	public Validator getVaidator()
-	{
+	public Validator getVaidator() {
 		Validator validator = new Validator();
 		return validator;
 	}
 
 	@Bean
-	public EmailService getEmailService()
-	{
+	public EmailService getEmailService() {
 		EmailService emailService = new GenericEmailServiceImpl();
 		return emailService;
 	}
 
 	@Bean
-	public JavaMailSender getJavaMailSender()
-	{
+	public JavaMailSender getJavaMailSender() {
 		JavaMailSender mailSender = new JavaMailSenderImpl();
 		return mailSender;
 	}
 
 	@Bean
-	public ConfigProperties configProperties()
-	{
+	public ConfigProperties configProperties() {
 		return new ConfigProperties();
 	}
 
 	@Bean
-	public SessionObject sessionObject()
-	{
+	public SessionObject sessionObject() {
 		return new SessionObject();
 	}
 
 	@Bean
-	public RedisStorage redisStorage()
-	{
+	public RedisStorage redisStorage() {
 		return new RedisStorage();
 	}
 
-	// @Beanss
-	// public RedisConnection redisConnection()
-	// {
-	// return new RedisConnection();
-	// }
-
-	// @Configuration
-	// @EnableRedisHttpSession
-	// public class Config
-	// {
-	private @Value("${spring.redis.host}") String redisHost;
-    private @Value("${spring.redis.port}") int redisPort;
-
-
 	@Bean
-	public LettuceConnectionFactory connectionFactory()
-	{
-        System.out.print("Connecting to Redis "+redisHost+":"+redisPort);
-        
-        return new LettuceConnectionFactory(redisHost,redisPort);
+	public LettuceConnectionFactory connectionFactory() {
+		System.out.print("Connecting to Redis " + redisHost + ":" + redisPort);
+
+		return new LettuceConnectionFactory(redisHost, redisPort);
 
 	}
-	// }
-
-//	@Bean
-//	public RedisHttpSessionConfiguration redisSession()
-//	{
-//		return new RedisHttpSessionConfiguration();
-//	}
-
-	// @Bean
-	// public HTTPRequestInterceptor myInterceptor()
-	// {
-	// return new HTTPRequestInterceptor();
-	// }
-	// @Bean
-	// public KMService getOpenKMService()
-	// {
-	// KMService kmService = new OpenKMServiceImpl();
-	// return kmService;
-	// }
-
-	// public static void main(String[] args) {
-	// SpringApplication.run(CommonMain.class, args);
-	// }
 }
--- src/main/java/com/iemr/admin/utils/km/KMService.java
+++
@@ -1,8 +0,0 @@
-package com.iemr.admin.utils.km;
-
-public interface KMService
-{
-	String getDocumentRoot();
-	
-	String createDocument(String documentPath, String filepath);
-}
